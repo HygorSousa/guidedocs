23fd60dd75b549b83521df45cd398d4aef3a5b86
Hygor de Sousa <hygor-araujo@hotmail.com>
2019-10-26 10:59:24 -0300

    mudança das cores padrão do casos de uso

--- src/main/java/com/unitins/guideDocs/models/Pessoa.java
+++ src/main/java/com/unitins/guideDocs/models/Pessoa.java
@@ -11,7 +11,7 @@ import java.util.Collection;
 import java.util.List;
 
 @Entity
-public class Pessoa extends AuditoriaModel implements UserDetails {
+public class    Pessoa extends AuditoriaModel implements UserDetails {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
