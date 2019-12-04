915dd1671beed85493abbed8eaf719d14a04c1b6
SrCascudo <johnheldercardoso@gmail.com>
2019-12-04 10:46:42 -0300

    Definir Disponibilidade - [back-end]

--- src/main/java/com/unitins/guideDocs/controllers/DefinirDisponibilidadeController.java
+++ src/main/java/com/unitins/guideDocs/controllers/DefinirDisponibilidadeController.java
@@ -1,20 +1,52 @@
 package com.unitins.guideDocs.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
 import com.unitins.guideDocs.models.Disponibilidade;
+import com.unitins.guideDocs.models.Professor;
+import com.unitins.guideDocs.repository.DefinirDisponibilidadeRepository;
+import com.unitins.guideDocs.repository.PessoaRepository;
 
 @Controller
 public class DefinirDisponibilidadeController {
-	Disponibilidade disponibilidade = new Disponibilidade();
 	
-	public Disponibilidade getDisponibilidade() {
-		return disponibilidade;
+	@Autowired
+	private DefinirDisponibilidadeRepository disponibilidadeDb;
+	
+	@Autowired
+	private PessoaRepository pessoaDb;
+	
+	@RequestMapping(method = RequestMethod.POST, value = "/addDisponibilidade")
+	public String addDisponibilidade(Disponibilidade disponibilidade) {
+		Authentication usuarioSessao = SecurityContextHolder.getContext().getAuthentication();
+		String login = usuarioSessao.getName();
+		Professor professor = (Professor) pessoaDb.findByCpf(login);
+	
+		disponibilidade.setProfessor(professor);
+		
+		disponibilidadeDb.save(disponibilidade);
+		
+		return "redirect:/definirDisponibilidade";
 	}
 	
-	private void set() {
-		// TODO Auto-generated method stub
-
+	@RequestMapping(method = RequestMethod.GET ,value = "/definirDisponibilidade")
+	public ModelAndView listarDisponibilidades() {
+		ModelAndView mv = new ModelAndView("/professor/definirDisponibilidade");
+		Iterable<Disponibilidade> disponibilidades = disponibilidadeDb.findAll();
+		mv.addObject("disponibilidades", disponibilidades);
+		return mv;
 	}
 	
+	@RequestMapping("/deletarDisponibilidade")
+	public String deletarDisponibilidade(Disponibilidade disponibilidade) {
+		
+		disponibilidadeDb.delete(disponibilidade);
+		return "redirect:/definirDisponibilidade";
+	}
 }
---
+++ src/main/java/com/unitins/guideDocs/repository/DefinirDisponibilidadeRepository.java
@@ -0,0 +1,9 @@
+package com.unitins.guideDocs.repository;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.unitins.guideDocs.models.Disponibilidade;
+
+public interface DefinirDisponibilidadeRepository extends CrudRepository<Disponibilidade, String>{
+	
+}
